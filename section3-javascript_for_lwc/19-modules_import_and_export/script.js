63fbe1e01f748730e7a61dd505a538c46efa5f03
Leonardo Brito <leonardo.bds@gmail.com>
2022-07-26 10:54:00 -0300

    Section3: Query Selector

---
+++ section3-javascript_for_lwc/19-modules_import_and_export/script.js
@@ -0,0 +1,6 @@
+//import minus, {PI, add} from './utils.js';
+import * as UTILS from './utils.js'
+
+console.log(UTILS.PI);
+console.log(UTILS.add(2,3));
+console.log(UTILS.minus(8 ,5));
---
+++ section3-javascript_for_lwc/19-modules_import_and_export/utils.js
@@ -0,0 +1,16 @@
+// Normal Export
+const PI_DATA = 3.14;
+function add(a, b){
+    console.log(a + b);
+}
+
+// export together
+// export {PI, add};
+
+// export with alias
+export {PI_DATA as PI, add};
+
+// export with default
+export default function minus(a, b){
+    console.log(a - b);
+}
