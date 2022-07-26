1bab8049942f0d4ee800ff9ce458deefc26c668e
Leonardo Brito <leonardo.bds@gmail.com>
2022-07-26 09:03:28 -0300

    Section3: String Interpolation

---
+++ section3-javascript_for_lwc/14-string_interpolation/script.js
@@ -0,0 +1,25 @@
+// String interpolation
+var name = "Leonardo";
+var age = "37";
+
+
+var str = "my name is " + name + " and my age is " + age;
+console.log(str);
+
+var a = 3;
+var b = 7;
+
+var str1 = "the sum of: " + a + " and " + b + " is: " + a+b;
+console.log(str1);
+console.log(a+b);
+
+var name = "Leonardo";
+var age = 37;
+console.log(`My name is ${name}`);
+
+var c = 10;
+var d = 20;
+console.log(`the sum of ${a} + ${b} is ${a+b}`);
+
+var recordId = 123456789;
+console.log(`https://www.salesforce.com/${recordId}`);
