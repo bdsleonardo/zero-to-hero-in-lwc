f86ba4f31d8fe144b5808882a1fcf1462c548c5f
Leonardo Brito <leonardo.bds@gmail.com>
2022-07-26 21:04:55 -0300

    Section5: 35.@track properties

--- section4-lwc_and_setup/lwc_learning/force-app/main/default/lwc/helloWorld/helloWorld.js
+++ section4-lwc_and_setup/lwc_learning/force-app/main/default/lwc/helloWorld/helloWorld.js
@@ -1,4 +1,4 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, track } from 'lwc';
 
 export default class HelloWorld extends LightningElement {
     fullName = 'Zero to Hero';
@@ -6,5 +6,19 @@ export default class HelloWorld extends LightningElement {
 
     changeHandler(event){
         this.title = event.target.value;
+    }
+
+    @track
+    address = {
+        city: 'Melbourne',
+        postCode: 3008,
+        country: 'Australia'
+    };
+
+    userList = ['a', 'b', 'c'];
+    trackHandler(event){
+        //this.address = {...this.address, "city": event.target.value};
+        this.address.city = event.target.value;
+        this.userList[0] = '';
     }
 }
